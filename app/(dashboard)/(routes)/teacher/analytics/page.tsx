4ad7d8999d839fe466d44bfb58788d7ff842a640
Remus Victuelles <remus.victuelles@gmail.com>
2023-10-07 01:00:22 +1100

    Implemented Login, Dashboard, Teacher view and Add Course

---
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -0,0 +1,9 @@
+const AnalyticsPage = () => {
+   return ( 
+      <div className="p-6">
+         Analytics!!
+      </div>
+    );
+}
+ 
+export default AnalyticsPage;
---
+++ app/test/page.tsx
@@ -0,0 +1,6 @@
+
+const TestPage = () => {
+   return ( <div className="h-full flex items-center justify-center">This should be unprotected.</div> );
+}
+ 
+export default TestPage;
